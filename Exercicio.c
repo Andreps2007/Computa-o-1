40ec30f4eda4554a40b97d432becad4d4f103a66
Andreps2007 <andrepiccoli@alunos.utfpr.edu.br>
2025-04-02 09:39:30 -0300

    Exercicio1.c

---
+++ Exercicio.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+
+    float oi,tchau,hora, min, seg;
+
+    printf("Digite hora, minuto e segundo: ");
+    scanf("%f %f %f", &hora, &min, &seg);
+    tchau = (hora*3600 + min*60 + seg);
+    oi = (min + seg/60);
+
+    if (hora>1 && min>15)
+    {
+        printf("Quantidade de segundos: %f\n ", tchau);
+
+    }
+    else
+    {
+        printf("Quantidade de minutos: %f\n", oi);
+
+    }
+    printf(" Horario : %0.0f:%0.0f:%0.0f\n", hora, min, seg);
+
+    return 0;
+
+
+}
