3897dded61087d3e9bff48a53860628f9ff51b0b
Andreps2007 <andrepiccoli@alunos.utfpr.edu.br>
2025-04-09 10:49:10 -0300

    Create Exercicio 2.c

---
+++ Exercicio 2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main ()
+{
+    int i = 1, n;
+    printf("Digite um Numero: ");
+    scanf("%d", &n);
+    while (100>=i) {
+
+    if (i%n == 2) {
+    printf("%d\n", i);
+}
+    i++;
+}
+
+return 0;
+
+
+
+}
