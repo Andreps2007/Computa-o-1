cc0e39109be750516da6c0c4595b66261f81f0dd
Andreps2007 <andrepiccoli@alunos.utfpr.edu.br>
2025-04-29 20:16:53 -0300

    tabuado.c

---
+++ tabuado.c
@@ -0,0 +1,16 @@
+int main() {
+    int i= 1,n,soma;
+
+    
+    
+    printf("Digite um numero:\n");
+    scanf("%d", &n);
+    while(i<=10){
+     soma= n*i;
+    printf ("%d X %d =  %d\n",n, i, soma);
+        i= i+1;
+    }
+
+
+
+return 0; }
