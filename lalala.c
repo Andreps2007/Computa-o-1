8fb467808b77090931bc0a8a01f71a48e4791de3
Andreps2007 <andrepiccoli@alunos.utfpr.edu.br>
2025-05-14 11:01:01 -0300

    Create lalala.c

---
+++ lalala.c
@@ -0,0 +1,13 @@
+include<stdio.h>
+
+#define TAMANHO 5
+
+
+void definevetor(int vetor[TAMANHO], int i){
+for(i=0 ; i<=5; i+1)
+vetor [i]= rand()%100;}
+void imprimevetor(int vetor[TAMANHO],int i){
+    for(i=0;i<=5;i++)
+        printf("os valores sao %d\n", vetor[i]);}
+
+        int main(){}
