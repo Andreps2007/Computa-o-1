264496efebf1d9ad13c200f2081507bfdcfdc5d9
Andreps2007 <andrepiccoli@alunos.utfpr.edu.br>
2025-05-28 10:03:46 -0300

    iniciofazburro.c

---
+++ iniciofazburro.c
@@ -0,0 +1,21 @@
+include <stdio.h>
+#define TAM 10
+
+void imprimematriz(int ms[TAM][TAM],int nl. int nc)
+{
+    for(nl=0; nl<TAM; nl++)
+    {
+        for(nc=0; nc<TAM; nl++)
+        {
+
+        } printf("%d",mn[nl][nc];)
+
+    }
+    printf("\n");
+}
+
+int main(){
+
+
+
+}
