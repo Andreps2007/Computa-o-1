f458062c3d65f7e9041a9840f1f754b48df5dd75
Andreps2007 <andrepiccoli@alunos.utfpr.edu.br>
2025-06-11 10:39:06 -0300

    Create terminar string.c

---
+++ terminar string.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+int main()
+{
+    int i=1;
+    int p;
+    char nomes [3] [200];
+    char frase2 [200];
+    printf("Digite os 3 nomes\n");
+    for( int j=0; j<3; j++)
+    {
+        fgets(nomes[j],sizeof nomes[i], stdin);
+
+    }
+
+
+
+    for(p=0; p<3; p++)
+    {
+        printf("Digite o nome
+        fgets(frase2,sizeof frase2, stdin);
+
